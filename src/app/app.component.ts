18e409cd519e52b7f2678ee2daeb0ebf5cecbf5d
Bogdan Kovalenko <kovalbogdan95@gmail.com>
2017-09-13 21:26:21 +0300

    Setup semantic-ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+        console.log(`Add article ${title.value} and ${link.value}`);
+        return false;
+    }
+
 }
