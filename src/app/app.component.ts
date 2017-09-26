009543ab3485c280ade5edf2658d05ee6aeeb56a
Bogdan Kovalenko <kovalbogdan95@gmail.com>
2017-09-26 22:00:50 +0300

    Add vote sort feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,6 +25,11 @@ export class AppComponent {
       title.value = '';
       link.value = '';
       return false;
+
+  }
+
+  sortedArticles(): Article[] {
+      return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
   }
 
 }
